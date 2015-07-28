32b617a6d837bad5369a9551df64356db30b4606
mon_ye <evan9669@sina.com>
2015-07-28 11:07:00 +0800

    Update standardclient.py
    this py file could be used as a client. Strings can be sent by it using the standardserver.py as well. Update in 11:00 in July 28.

--- standardclient.py
+++ standardclient.py
@@ -1,22 +1,34 @@
 import socket,sys
 
-port = 11111
 host = sys.argv[1]
+port = int(sys.argv[2])
+
+print 'please input:'
+mess = raw_input()
+#input message
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#create a socket
 
 try:
 	s.connect((host,port))
+	#connect to server
 except socket.gaierror,e:
 	print "Error connecting to server : %s" % e
 	sys.exit(1)
-	
-	
-s.sendall('hello server' + "\r\n")
+
+try:
+	s.sendall(mess)
+	s.shutdown(1)
+	#send message
+except (KeyboardInterrupt,SystemExit):
+	raise
+except:
+	traceback.print_exc()
 
 while 1:
 	buf = s.recv(2048)
 	if not len(buf):
 		break
 	sys.stdout.write(buf)
-	
+	#read message from server and print it
