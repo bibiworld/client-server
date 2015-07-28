96b4a84533213c80a371fa081e93f16055702d3c
mon_ye <evan9669@sina.com>
2015-07-28 10:49:44 +0800

    Update standardserver.py
    this py file could be used as a server. Strings can be sent by it using the standardclient.py as well. Update in 11:00 in July 28.

--- standardserver.py
+++ standardserver.py
@@ -1,16 +1,21 @@
-import socket,traceback,sys,time
+import socket,traceback
 
 host = ''
 port = 11111
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#create a socket
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+#set options
 s.bind((host,port))
+#bind to host and port 
 s.listen(1)
+#set number of client waiting in the queue
 
 while 1:
 	try:
 		clientsock,clientaddr = s.accept()
+		#accept socket from client
 	except KeyboardInterrupt:
 		raise
 	except:
@@ -19,7 +24,42 @@ while 1:
 
 	try:
 		print 'got connection from',clientsock.getpeername()
-		clientsock.sendall('hello\n')
+		#print client info
+	except (KeyboardInterrupt,SystemExit):
+		raise
+	except:
+		traceback.print_exc()
+
+	try:
+		fs = clientsock.makefile('rw',0)
+		#create a file object
+		print 'makefile()'
+	except (KeyboardInterrupt,SystemExit):
+		raise
+	except:
+		traceback.print_exc()
+
+	try:
+		mess = fs.readline().strip()
+		print 'client wrote:',mess
+		#get message from client
+	except (KeyboardInterrupt,SystemExit):
+		raise
+	except:
+		traceback.print_exc()
+
+	try:
+		fs.write('welcome' + str(clientsock.getpeername()) + '\n')
+		fs.write('you have written:' + mess + '\n')
+		fs.flush()
+		#send message
+	except (KeyboardInterrupt,SystemExit):
+		raise
+	except:
+		traceback.print_exc()
+
+	try:
+		fs.close()
 	except (KeyboardInterrupt,SystemExit):
 		raise
 	except:
